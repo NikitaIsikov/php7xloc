737cd442221f99684daf032ad7d3a1eed7d45dc1
nikita isikov <nikitaisikov90@gmail.com>
2020-02-03 11:58:12 +0200

    homework task 1

--- classes/Employee.php
+++ classes/Employee.php
@@ -4,7 +4,13 @@ namespace classes;
 
 class Employee
 {
-    public $name;
-    public $age;
-    public $salary;
+    public string $name;
+    public int $age;
+    public int $salary;
+
+    public function newEmp($name, $age, $salary) {
+        $this->name = $name;
+        $this->age = $age;
+        $this->salary = $salary;
+    }
 }
--- index.php
+++ index.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$employee = new classes\Employee();
-$employee->name = 'Petya';
-echo $employee->name;
+$emp1 = new classes\Employee();
+$emp2 = new classes\Employee();
+$emp1->newEmp('Ivan', 25, 1000);
+$emp2->newEmp('Vasya', 26, 2000);
+
+echo "<p>1.4: " . "$emp1->salary + $emp2->salary";
+echo " 1.5: " . "$emp1->age + $emp2->age" . "</p>";
