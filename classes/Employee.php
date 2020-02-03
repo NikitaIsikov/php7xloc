5658eda1600468a2ae4ae7db6911483c10e3c3d9
nikita isikov <nikitaisikov90@gmail.com>
2020-02-03 15:46:10 +0200

    Done

--- classes/Employee.php
+++ classes/Employee.php
@@ -7,10 +7,32 @@ class Employee
     public string $name;
     public int $age;
     public int $salary;
+    public bool $adult;
 
-    public function newEmp($name, $age, $salary) {
+    public function newEmp($name, $age, $salary)
+    {
         $this->name = $name;
         $this->age = $age;
         $this->salary = $salary;
+    }
+    public function getName($name)
+    {
+        $this->name = $name;
+    }
+    public function getAge($age)
+    {
+        $this->age = $age;
+        $this->checkAge($age);
+    }
+    public function getSalary($salary, $otherEmp = null)
+    {
+        if ($otherEmp === null) {
+            return $this->salary = $salary;
+        } elseif($salary === 'sum') {
+            return $this->salary + $otherEmp->salary;
+        }
+    }
+    public function checkAge($age) {
+        $this->adult = ($age >= 18) ? true : false;
     }
 }
--- index.php
+++ index.php
@@ -3,11 +3,25 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/vendor/autoload.php';
-
+//task1
 $emp1 = new classes\Employee();
 $emp2 = new classes\Employee();
 $emp1->newEmp('Ivan', 25, 1000);
 $emp2->newEmp('Vasya', 26, 2000);
 
-echo "<p>1.4: " . "$emp1->salary + $emp2->salary";
-echo " 1.5: " . "$emp1->age + $emp2->age" . "</p>";
+echo '1.4: ' . ($emp1->salary + $emp2->salary) . '<br>';
+echo '1.5: ' . ($emp1->age + $emp2->age) . '<br><br>';
+//task2
+$emp3 = new classes\Employee();
+$emp4 = new classes\Employee();
+$emp3->getName('Vlad');
+$emp3->getAge(17);
+$emp3->getSalary(700);
+$emp4->getName('Denis');
+$emp4->getAge(23);
+$emp4->getSalary(1500);
+
+echo '2.5: ' . $emp3->name . "'s age bool is '" . $emp3->adult . "'<br>";
+echo $emp4->name . "'s age bool is '" . $emp4->adult . "'<br>";
+echo '2.6: Salary sum =' . $emp3->getSalary('sum', $emp4) . '<br>';
+
